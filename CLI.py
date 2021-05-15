6ecf0dfea7d4bf1daedb3001cfc56b696d7d4e11
Perfectrum <38108124+Perfectrum@users.noreply.github.com>
2021-05-15 13:54:10 +0700

    Update CLI.py

--- CLI.py
+++ CLI.py
@@ -10,18 +10,12 @@ parser.add_argument('--init',
                     action = 'store',
                     help = 'Инициализация')
 
-subparsers = parser.add_subparsers()
-add_card_parser = subparsers.add_parser('add_card',
-                                        help = 'Добавить карточку')
-add_card_parser.add_argument('card_name',
-                                action='store',
-                                help='Название карточки')
-add_card_parser.add_argument('question_path',
-                                action='store',
-                                help='Путь к файлу с вопросом')
-add_card_parser.add_argument('answer_path',
-                                action='store',
-                                help='Путь к файлу с ответом')
+parser.add_argument('--add_card',
+                    action='append',
+                    nargs = 3,
+                    metavar=('card_name', 'question_path', 'answer_path'),
+                    help = 'Добавить карточку')
+
 
 parser.add_argument('--delete_card',
                     metavar='card_name',
@@ -39,16 +33,12 @@ if args.init:
         print(add_user(username))
 
 # Добавление карточки
-args_vars = vars(args)
-if 'card_name' in args_vars.keys() and \
-    'question_path' in args_vars.keys() and \
-    'answer_path' in args_vars.keys():
+if args.add_card:
     username = os.getlogin()
-    status, message = add_card(
-		            username,
-		            args_vars['card_name'],
-		            args_vars['question_path'],
-		            args_vars['answer_path'])
+    card_name = args.add_card[0][0]
+    question_path = args.add_card[0][1]
+    answer_path = args.add_card[0][2]
+    status, message = add_card(username, card_name, question_path, answer_path)
     print(message)
 
 # Удаление карточки
