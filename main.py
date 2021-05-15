2a9ab7bd6f4df706bf22ca582c140b04fcc1bbfc
Stormlit <41117156+Palkowski@users.noreply.github.com>
2021-05-15 07:11:40 +0700

    insert и delete card

--- main.py
+++ main.py
@@ -7,7 +7,44 @@ import sqlite3
 # Функция, создающая файловую структуру в заданной директори, #
 #                        запускается при первом использовании #
 ###############################################################
-# TODO: создавать БД
+
+
+# Запись карт очки в БД
+def insert_card(user_id, card_name, question, answer):
+    main_connection = sqlite3.connect("main.db")
+    main_cursor = main_connection.cursor()
+    main_cursor.execute("SELECT * FROM cards WHERE card_name = \""+ str(card_name) +"\" AND user_id = " + str(user_id))
+    if main_cursor.fetchall() == []:
+        main_cursor.execute("SELECT card_id FROM cards")
+        cards_data = [(user_id, card_name, question, answer)]        
+        main_cursor.execute("INSERT INTO cards (user_id, card_name, question, answer) VALUES ("+
+                            str(user_id)+",\""+str(card_name)+"\",\""+str(question)+"\",\""+str(answer)+"\")")
+        main_connection.commit()
+        main_connection.close()
+    else:
+        main_connection.close()
+        print("Card with this name already exists")
+        return "Card with this name already exists"
+
+
+# Удаление карт очки из БД
+def delete_card(user_id, card_name):
+    main_connection = sqlite3.connect("main.db")
+    main_cursor = main_connection.cursor()
+    main_cursor.execute("SELECT * FROM cards WHERE card_name = \""+ str(card_name) +"\" AND user_id = " + str(user_id))
+    if main_cursor.fetchall() == []:
+        print("There is no card with this name")
+        main_connection.close()
+        return "There is no card with this name"
+    else:
+        main_cursor.execute("SELECT card_id FROM cards WHERE card_name = \""+ str(card_name) +"\" AND user_id = " + str(user_id))
+        delid = main_cursor.fetchall()[0][0]
+        main_cursor.execute("DELETE FROM cards WHERE card_name = \""+ str(card_name) +"\" AND user_id = " + str(user_id))
+        main_cursor.execute("UPDATE cards SET card_id = card_id - 1 WHERE card_id > "+str(delid))
+        main_connection.commit()
+        main_connection.close()
+
+
 def init(directory):
 
     config = configparser.ConfigParser()
