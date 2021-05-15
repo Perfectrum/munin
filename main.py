351dd4af0053ea1a867ffce208b720b1dab11975
Perfectrum <38108124+Perfectrum@users.noreply.github.com>
2021-05-15 07:57:31 +0700

    Работа с файлами + работа с БД

--- main.py
+++ main.py
@@ -1,50 +1,14 @@
 import os
+import shutil
 from glob import glob
 import configparser
 import sqlite3
+
 
 ###############################################################
 # Функция, создающая файловую структуру в заданной директори, #
 #                        запускается при первом использовании #
 ###############################################################
-
-
-# Запись карт очки в БД
-def insert_card(user_id, card_name, question, answer):
-    main_connection = sqlite3.connect("main.db")
-    main_cursor = main_connection.cursor()
-    main_cursor.execute("SELECT * FROM cards WHERE card_name = \""+ str(card_name) +"\" AND user_id = " + str(user_id))
-    if main_cursor.fetchall() == []:
-        main_cursor.execute("SELECT card_id FROM cards")
-        cards_data = [(user_id, card_name, question, answer)]        
-        main_cursor.execute("INSERT INTO cards (user_id, card_name, question, answer) VALUES ("+
-                            str(user_id)+",\""+str(card_name)+"\",\""+str(question)+"\",\""+str(answer)+"\")")
-        main_connection.commit()
-        main_connection.close()
-    else:
-        main_connection.close()
-        print("Card with this name already exists")
-        return "Card with this name already exists"
-
-
-# Удаление карт очки из БД
-def delete_card(user_id, card_name):
-    main_connection = sqlite3.connect("main.db")
-    main_cursor = main_connection.cursor()
-    main_cursor.execute("SELECT * FROM cards WHERE card_name = \""+ str(card_name) +"\" AND user_id = " + str(user_id))
-    if main_cursor.fetchall() == []:
-        print("There is no card with this name")
-        main_connection.close()
-        return "There is no card with this name"
-    else:
-        main_cursor.execute("SELECT card_id FROM cards WHERE card_name = \""+ str(card_name) +"\" AND user_id = " + str(user_id))
-        delid = main_cursor.fetchall()[0][0]
-        main_cursor.execute("DELETE FROM cards WHERE card_name = \""+ str(card_name) +"\" AND user_id = " + str(user_id))
-        main_cursor.execute("UPDATE cards SET card_id = card_id - 1 WHERE card_id > "+str(delid))
-        main_connection.commit()
-        main_connection.close()
-
-
 def init(directory):
 
     config = configparser.ConfigParser()
@@ -71,8 +35,9 @@ def init(directory):
         message = 'OK, directory {} is created and set'.format(directory)
 
     if status:
-        # База данных
-        main_connection = sqlite3.connect("main.db")
+        
+        # Создаем базу данных пользователей и карточек
+        main_connection = sqlite3.connect('{}/main.db'.format(directory))
         main_cursor = main_connection.cursor()
         # Таблица с карточками в БД
         main_cursor.execute("""CREATE TABLE IF NOT EXISTS cards
@@ -82,19 +47,19 @@ def init(directory):
                    question TEXT NOT NULL, 
                    answer TEXT NOT NULL)""")  
         main_connection.commit()
-        # Таблица с пользователями и id в БД
+         # Таблица с пользователями и id в БД
         main_cursor.execute("""CREATE TABLE IF NOT EXISTS users
                   (user_id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                    user_name TEXT NOT NULL)""")  
         main_connection.commit()
         main_connection.close()
-        # pass # Создать БД тут
-    
+
     # Запоминаем изменения в конфиге
     with open('config.ini', 'w') as config_file:
         config.write(config_file)
 
-    return message
+    return status, message
+
 
 ##########################################################
 # Функция, добавляющая пользователя в файловую структуру #
@@ -107,15 +72,139 @@ def add_user(username):
     main_dir = config['main']['main_directory']
 
     # добавить пользователя в БД тут
-
+    
+    # Создаем папку с карточками
     os.mkdir('{}/{}'.format(main_dir, username))
 
     return 'Добавлен пользователь {}'.format(username)
 
 
-if __name__ == '__main__':
-    directory = input('Введи директорию:\n')
+##############################################
+# Функция, добавляющая пользователю карточку #
+##############################################
+def add_card(username, card_name, question_path, answer_path, additional_files_paths=[]):
 
-    init_status = init(directory)
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    main_dir = config['main']['main_directory']
+
+    # Если карточка с таким названием уже есть, отказ
+    if False: # вот тут проверка существования карточки в БД
+        status = False
+        message = 'A card with this name alreday exists'
+    # Если карточки с таким названием нет, пытаемся ее добавить
+    else:
+        message = ''
+        status = True
+
+        # Если какой-то из путей не валидный, отказ
+        files_paths = additional_files_paths
+        files_paths.append(question_path)
+        files_paths.append(answer_path)
+
+    # Подключаемся к БД
+    main_connection = sqlite3.connect('{}/main.db'.format(main_dir))
+    main_cursor = main_connection.cursor()
+
+    # Проверяем существование карточки в БД
+    ######################
+    # ОПРЕДЕЛИТЬ user_id #
+    ######################
+    main_cursor.execute("SELECT * FROM cards WHERE card_name =\""+
+                        str(card_name) +"\" AND user_id = " + str(user_id))
+    # Если карточки нет в БД, пробуем ее создать
+    if main_cursor.fetchall() == []:
+   
+        # Проверяем существование файлов
+        for file_path in files_paths:
+            if os.path.exists(file_path):
+                 message+= 'File {} is OK\n'.format(file_path)
+            else:
+                message+= 'File {} does not exist\n'.format(file_path)
+                status = False
+		
+        # Если с путями все в порядке, их надо добавить в нужные директории
+        if status:
+		    # Проверяем, в правильной ли директории лежат файлы
+            for file_path in files_paths:
+                cur_right_path = '{}/{}/{}'.format(main_dir, username, card_name)
+		        # Если файл лежит правильно, не трогаем его
+                if file_path[:file_path.rindex('/')] == cur_right_path:
+                    message+= 'File {} in the right folder\n'.format(file_path)
+                else:
+                    # Если файл лежит неправильно, копируем куда надо
+                    if not os.path.exists(cur_right_path): os.mkdir(cur_right_path)
+                    shutil.copyfile(file_path, '{}/{}'.format(
+		                                                    cur_right_path,
+		                                                    file_path.split('/')[-1]))
+                    message+= 'File {} in the wrong folder. Copied to {}\n'.format(
+		                                        file_path, cur_right_path)
+            # Добавляем карточку в БД
+            main_cursor.execute("SELECT card_id FROM cards")
+            cards_data = [(user_id, card_name, question, answer)]        
+            main_cursor.execute(
+                "INSERT INTO cards (user_id, card_name, question_path, answer_path) VALUES ("+
+		        str(user_id ) +
+                ",\" " +
+                str(card_name) +
+                "\",\"" +
+                str(question_path) +
+                "\",\" " +
+                str(answer_path)+"\")")
+            main_connection.commit()
+        else:
+            status = False
+            message+= 'Card {} is already exists'.format(crad_name)	
+        
+    main_connection.close()
+
+    if status:
+        message+= 'Card {} is added to the user {}'.format(card_name, username)
+
+    return status, message
+
+
+###############################
+# Функция, удаляющая карточку #
+###############################
+def delete_card(username, card_name):
     
-    print(init_status)
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    main_dir = config['main']['main_directory']
+
+    #  Подключаемся к БД
+    main_connection = sqlite3.connect('{}/main.db'.format(main_dir))
+    main_cursor = main_connection.cursor()
+	
+    # Если такой карточки нет, отказ
+    ######################
+    # ОПРЕДЕЛИТЬ user_id #
+    ######################
+    main_cursor.execute("SELECT * FROM cards WHERE card_name = \""+ str(card_name) +"\" AND user_id = " + str(user_id))
+    if main_cursor.fetchall() == []:
+        status = False
+        message = 'Card {} does not exist'.format(card_name)
+    # Если такая карточка существует, удаляем ее
+    else:
+        main_cursor.execute("SELECT card_id FROM cards WHERE card_name = \""+ str(card_name) +"\" AND user_id = " + str(user_id))
+        delid = main_cursor.fetchall()[0][0]
+        main_cursor.execute("DELETE FROM cards WHERE card_name = \""+ str(card_name) +"\" AND user_id = " + str(user_id))
+        main_cursor.execute("UPDATE cards SET card_id = card_id - 1 WHERE card_id > "+str(delid))
+        main_connection.commit()
+        status = True
+    main_connection.close()
+
+
+    path = '{}/{}/{}'.format(main_dir, username, card_name)
+    # Если карточка существует, удаляем ее
+    if os.path.exists(path):
+        shutil.rmtree(path)
+        status = True
+        message = 'Card {} is deleted'.format(card_name)
+    # Если такой карточки нет, отказ
+    else:
+        status = False
+        message = 'Card {} does not exist'.format(card_name)
+    
+    return status, message
